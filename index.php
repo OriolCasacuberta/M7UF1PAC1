1a4d223d3341734b6d05772d8ad4a18ea5933208
OriolCasacuberta <114771512+OriolCasacuberta@users.noreply.github.com>
2024-01-12 10:45:24 +0100

    icon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>La meva web</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/jpg" href="./img/LogoIsitec.png"/>
     </head>
     <body>
         <p>La meva web: Oriol-Mario</p>
