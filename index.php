4c47912844030c5ee8140aea6b34297f5877f4e4
OriolCasacuberta <114771512+OriolCasacuberta@users.noreply.github.com>
2024-01-19 10:50:09 +0100

    canvis en css

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!--PHP-->
 <?php
-    //menssaje de error
+    // Mensaje de error
     $error = '';
 
-    //nos asseguramos que el metodo de REQUEST se POST
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Nos asseguramos que el metodo de REQUEST se POST
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
 
-        if(count($_POST)==2){
-            //filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL) =VERIFICA QUE SIGUI UN EMAIL I EVITA INJECCIONS DE DADES
+        if(count($_POST)==2)
+        {
+            // Filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL) = VERIFICA QUE SIGUI UN EMAIL I EVITA INJECCIONS DE DADES
             $user   = isset($_POST["user"]) ? filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL) : "";
             $pass   = isset($_POST["pass"]) ? filter_input(INPUT_POST,'pass',FILTER_SANITIZE_STRING) : "";
             $logged = verificaUsuari($mail,$pass);
             
-            if($logged!==false){
-                //creem la sessio
+            if($logged!==false)
+            {
+                // Creem la sessio
                 session_start();
                 
                 //$_SESSION['user'] = $logged['idUsuari'];
                 //$_SESSION['pass'] = $logged['name']; 
-                //fem la redireccio
+                // Fem la redireccio
                 header("Location: ./php/home.php");
-                //exit()= recomendable por la redireccion de ficheros
+                // Exit() = recomendable por la redireccion de ficheros
                 exit();
-            }else{
-                $error = "Revisa l'adreça de correu/username i/o la contrasenya";
             }
+            
+            else $error = "Revisa l'adreça de correu/username i/o la contrasenya";
 
         }
-        else{
-            $error = "User or Password are require!";
-        }
-
-
+        
+        else $error = "User or Password are require!";
     }
 ?>
 
@@ -44,6 +44,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/jpg" href="./img/LogoIsitec.png"/>
         <link rel="stylesheet" href="./css/estils.css">
+        <link rel="stylesheet" href="./css/index.css">
     </head>
     <body>
         <main>
