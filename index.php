b5e4edf461d58bc7f150e2a18bb43264c24ab952
OriolCasacuberta <114771512+OriolCasacuberta@users.noreply.github.com>
2024-01-19 08:41:35 +0100

    css

--- index.php
+++ index.php
@@ -5,10 +5,40 @@
         <title>La meva web</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/jpg" href="./img/LogoIsitec.png"/>
+        <link rel="stylesheet" href="./css/estils.css">
     </head>
     <body>
         <main>
-            <img src="./img/LogoIsitec.png" alt="LogoIsitec">
+            <form method = "post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>">
+
+                <!-- MAIL -->
+                <label for="user">Correu Electrònic</label>
+                <br>
+                <input id="user" name="user" type="email" placeholder="Introdueix el teu correu electrònic" autofocus required>
+
+                <br>
+                <br>
+
+                <!-- CONTRASENYA -->
+                <label for="contrasenya">Contrasenya</label>
+                <br>
+                <input id="contrasenya" name="contrasenya" type="password" placeholder="Introdueix el teu password" required>
+
+                <br>
+                <br>
+
+                <!-- SUBMIT -->
+                <button class="button" type="submit"><span>Login</span></button>
+
+                
+
+            </form>
         </main>
+
+        <aside>
+            <p>Encara no tens un compte?</p>
+        </aside>
+
+        <button class="button" type="submit"><span>Sign in</span></button>
     </body>
 </html>
